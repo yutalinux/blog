230faf1096386a825f57a348bac60b702afb45d3
Yuta <yutalinux@users.noreply.github.com>
2024-10-17 21:09:26 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://yutalinux.github.io",
   base: "/blog",
+  integrations: [tailwind()],
 });
