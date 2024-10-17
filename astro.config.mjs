ac74dc9314af21397d69e5ac641b3cc2b452fd67
yuta <jp.yuta0131@gmail.com>
2024-10-18 04:03:09 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,25 @@ export default defineConfig({
   site: "https://yutalinux.github.io",
   base: "/blog",
   integrations: [tailwind(), sitemap(), icon()],
+  markdown: {
+    rehypePlugins: [
+      "rehype-slug",
+      ["@jsdevtools/rehype-toc", { headings: ["h2", "h3", "h4"] }],
+      [
+        "rehype-autolink-headings",
+        {
+          behavior: "append",
+          content: {
+            type: "element",
+            tagName: "i",
+            properties: {
+              className: ["heading-anchor", "fa", "fa-link"],
+            },
+            children: [],
+          },
+        },
+      ],
+      ["@jsdevtools/rehype-toc", { headings: ["h2", "h3", "h4"] }],
+    ],
+  },
 });
